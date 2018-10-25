d1944095eaa41a02932ad8337bb8bd4762749a13
Toexport <chibizhangpeng@163.com>
2018-10-25 23:24:08 +0800

    新建项目
    现在才是正常的

---
+++ BOHAN-Custom/BOHAN-Custom/Main/BCTabBarController.h
@@ -0,0 +1,17 @@
+//
+//  BCTabBarController.h
+//  BOHAN-Custom
+//
+//  Created by 赵宁 on 2018/10/25.
+//  Copyright © 2018年 张鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BCTabBarController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BOHAN-Custom/BOHAN-Custom/ViewController/List /ListViewController.m
@@ -0,0 +1,23 @@
+//
+//  ListViewController.m
+//  BOHAN-Custom
+//
+//  Created by summer on 2018/10/25.
+//  Copyright © 2018 summer. All rights reserved.
+//
+
+#import "ListViewController.h"
+#import "PrefixHeader.pch"
+@interface ListViewController ()
+
+@end
+
+@implementation ListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = Localize(@"设备列表");
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
